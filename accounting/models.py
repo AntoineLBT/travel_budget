dd91620936434902bd5407bd4591ca4996fe276a
AntoineLBT <antoine.lebout@gmail.com>
2022-09-20 23:44:01 +0200

    Add Budget model and tests

--- accounting/models.py
+++ accounting/models.py
@@ -2,12 +2,25 @@ from datetime import date
 from uuid import UUID, uuid4
 
 from django.db import models
+from django.utils import timezone
 
 
-# Create your models here.
+class Budget(models.Model):
+    id: UUID = models.UUIDField(primary_key=True, default=uuid4)
+    name: str = models.CharField(
+        name="name",
+        max_length=255,
+        default=f"Budget-{timezone.now().strftime('%m/%d/%Y-%H:%M:%S')}",
+        unique=True,
+    )
+
+
 class Expense(models.Model):
 
     id: UUID = models.UUIDField(primary_key=True, default=uuid4)
     amount: float = models.FloatField(name="amount", default=0)
     label: str = models.CharField(name="label", max_length=255, default="")
-    date: date = models.DateField(name="date")
+    expense_date: date = models.DateField(name="date")
+    budget: Budget = models.ForeignKey(
+        Budget, on_delete=models.CASCADE, default=None
+    )
--- accounting/tests/test_models.py
+++ accounting/tests/test_models.py
@@ -1,14 +1,28 @@
 from datetime import date
 
 from django.test import TestCase
+from django.utils import timezone
+from hamcrest import assert_that, contains_string, instance_of, is_
 
-from ..models import Expense
+from ..models import Budget, Expense
 
 
-# Create your tests here.
-class ExpenseTests(TestCase):
-    def test_representation(self) -> None:
-        expense = Expense.objects.create(
-            amount=100, label="Test", date=date(2022, 12, 31)
+class BudgetTests(TestCase):
+    def test_budget_model(self) -> None:
+        budget = Budget.objects.create()
+        assert_that(budget, is_(instance_of(Budget)))
+
+    def test_budget_default_name(self) -> None:
+        budget = Budget.objects.create()
+        assert_that(
+            budget.name, contains_string(timezone.now().strftime("%m/%d/%Y"))
         )
-        assert isinstance(expense, Expense)
+
+
+class ExpenseTests(TestCase):
+    def test_expense_model(self) -> None:
+        budget = Budget.objects.create()
+        expense = Expense.objects.create(
+            amount=100, label="Test", date=date(2022, 12, 31), budget=budget
+        )
+        assert_that(expense, is_(instance_of(Expense)))
