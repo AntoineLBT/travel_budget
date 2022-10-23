9bbe6462c02f9a3d98c3fde57c21f63bb801a939
AntoineLBT <antoine.lebout@gmail.com>
2022-10-23 14:14:23 +0200

    Fix theme install

--- www/views.py
+++ www/views.py
@@ -1,5 +1,14 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name: str = "home.html"
+
+
+class LoginView(TemplateView):
+    template_name: str = "login.html"
+
+
+def index(request):
+    return render(request, "dashboard/dashboard.html")
