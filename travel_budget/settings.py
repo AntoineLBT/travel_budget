9babcbf7266332811ec080d747a301088a78d790
AntoineLBT <antoine.lebout@gmail.com>
2024-03-26 21:31:52 +0100

    add allowed hosts

--- travel_budget/settings.py
+++ travel_budget/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['antoinelbt.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
